122e7ebbd8c29d50ba307f2e9ecc8cd30fabd47e
DiliniUdawaththa <dmadhushani611@gmail.com>
2023-08-28 16:41:19 +0530

    Initial commit

---
+++ src/main/scala/lab/Q2.scala
@@ -0,0 +1,18 @@
+package lab
+
+object Q2 {
+  def countLetterOccurrences(words : List[String]):Int = {
+    val letterCount = words.map(word => word.length)
+    val totalLetterCount = letterCount.reduce((w1,w2) => w1 + w2)
+
+    //Return total letter count
+    totalLetterCount
+  }
+
+  def main(args:Array[String]): Unit = {
+    val words = List("apple", "banana", "cherry", "date")
+
+    println("Total count of letter occurrences: " + countLetterOccurrences(words))
+  }
+
+}
